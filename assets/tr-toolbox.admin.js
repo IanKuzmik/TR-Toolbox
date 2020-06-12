7eaf3dd6d3b4dd96525fd3118becd0a4da094c9a
Ian <kuzmik951@gmail.com>
2020-06-12 12:11:02 -0600

    typo

--- assets/tr-toolbox.admin.js
+++ assets/tr-toolbox.admin.js
@@ -7,7 +7,7 @@ const button = document.getElementById( 'js-tr-toolbox-http-headers-edit-button'
 // check if were on the right page
 if (button != null) {
   button.addEventListener( 'click', function() {
-    if ( confirm( 'Editing these options can have serious side effects on your website. Do you unserstand the risks and wish to continue?'  ) ) {
+    if ( confirm( 'Editing these options can have serious side effects on your website. Do you understand the risks and wish to continue?'  ) ) {
       // hide edit button
       button.classList.add('tr-invisible');
       // remove 'disabled' attribute from fields
